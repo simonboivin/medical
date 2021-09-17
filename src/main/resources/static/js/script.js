2e9005d489dd3289a904e18f0d546171e7234f58
Simon Boivin <sfp.boivin@gmail.com>
2021-09-17 10:42:42 +0200

    Rename modal id

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,14 +1,14 @@
-var exampleModal = document.getElementById('exampleModal')
-exampleModal.addEventListener('show.bs.modal', function (event) {
+var deleteModal = document.getElementById('deleteModal')
+deleteModal.addEventListener('show.bs.modal', function (event) {
 
     var button = event.relatedTarget
 
     var recipient = button.getAttribute('dataid')
 
-    var modalMesssage = exampleModal.querySelector('.modal-message')
+    var modalMesssage = deleteModal.querySelector('.modal-message')
 
     modalMesssage.textContent = 'Voulez-vous supprimer le patient ' + recipient + '?'
-    var modalForm = exampleModal.querySelector('.modal-form')
+    var modalForm = deleteModal.querySelector('.modal-form')
     var modalFormLink = '/patients/delete/' + recipient
     modalForm.setAttribute("action", modalFormLink)
 })
